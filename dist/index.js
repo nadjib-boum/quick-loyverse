5b8c2a79c0ac784fb0988f1e2183428db6611eb2
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-14 20:33:52 +0400

    first commit

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const OAuthClient = require("intuit-oauth");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static("public"));
+app.use((0, cors_1.default)());
+app.set("view engine", "ejs");
+const memoryCache = {
+    auth_code: "",
+    access_token: "",
+};
+const oauthClient = new OAuthClient({
+    clientId: process.env.INTUIT_CLIENT_ID,
+    clientSecret: process.env.INTUIT_CLIENT_SECRET,
+    environment: process.env.INTUIT_ENVIRONEMENT,
+    redirectUri: `https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl`,
+});
+app.get("/", (req, res) => {
+    res.status(200).send("Hello API");
+});
+app.get("/authorize", (req, res) => {
+    const authUri = oauthClient.authorizeUri({
+        scope: [OAuthClient.scopes.Accounting],
+        state: "intuit-test",
+    });
+    res.status(200).send(authUri);
+});
+app.get("/callback", (req, res) => {
+    oauthClient
+        .createToken(req.url)
+        .then(function (authResponse) {
+        res.status(200).send(JSON.stringify(authResponse.getJson(), null, 2));
+    })
+        .catch(function (e) {
+        console.error(e);
+        res.status(500).send("ERROR");
+    });
+});
+const { PORT } = process.env;
+app.listen(PORT, async () => {
+    console.log("App Is Running on Localhost, PORT:", PORT);
+});
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const OAuthClient = require("intuit-oauth");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+app.set("view engine", "ejs");
+
+type MemoryCache = {
+  auth_code: string;
+  access_token: string;
+};
+
+const memoryCache: MemoryCache = {
+  auth_code: "",
+  access_token: "",
+};
+
+const oauthClient = new OAuthClient({
+  clientId: process.env.INTUIT_CLIENT_ID,
+  clientSecret: process.env.INTUIT_CLIENT_SECRET,
+  environment: process.env.INTUIT_ENVIRONEMENT,
+  redirectUri: `https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl`,
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello API");
+});
+
+app.get("/authorize", (req: Request, res: Response) => {
+  const authUri = oauthClient.authorizeUri({
+    scope: [OAuthClient.scopes.Accounting],
+    state: "intuit-test",
+  });
+  res.status(200).send(authUri);
+});
+
+app.get("/callback", (req: Request, res: Response) => {
+  oauthClient
+    .createToken(req.url)
+    .then(function (authResponse: any) {
+      res.status(200).send(JSON.stringify(authResponse.getJson(), null, 2));
+    })
+    .catch(function (e: any) {
+      console.error(e);
+      res.status(500).send("ERROR");
+    });
+});
+
+const { PORT } = process.env;
+
+app.listen(PORT, async () => {
+  console.log("App Is Running on Localhost, PORT:", PORT);
+});
