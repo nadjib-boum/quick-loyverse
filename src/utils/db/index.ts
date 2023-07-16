6a714c4812945f3e7262249443b3049d58a0d221
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-16 08:39:49 +0400

    downgrade prisma versiom

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "../../../node_modules/.prisma/client";
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../../../node_modules/.prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
