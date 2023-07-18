24f6577d47608bf9834b4ec15e14c3674faa0d3f
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-18 15:44:19 +0400

    add app page

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -1,6 +1,9 @@
-import type { Router } from "express";
+import type { NextFunction, Request, Response, Router } from "express";
 import { getAllCompanies } from "../controllers/companies";
 
 export default (router: Router) => {
+  router.get("/app", (req: Request, res: Response, next: NextFunction) => {
+    res.render("app");
+  });
   router.get("/companies", getAllCompanies);
 };
