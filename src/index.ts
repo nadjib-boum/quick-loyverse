09a33c43d61533a15fb0dcbb00d2576af9af8533
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-18 15:52:52 +0400

    set static files path

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(cors());
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use("/api", routes());
 app.use(ErrorMiddleware.logger, ErrorMiddleware.handler);
 
