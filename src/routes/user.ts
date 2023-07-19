47fd878ceab757743937cd1cd035417b23e164f4
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-19 18:10:33 +0400

    add validateLogin middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,9 @@
 import type { Router } from "express";
-import { authorize } from "../middlewares/user";
+import { authorize, validateLogin } from "../middlewares/user";
 import { login, authCallback, logout } from "../controllers/user";
 
 export default (router: Router) => {
-  router.post("/user/login", login);
+  router.post("/user/login", validateLogin, login);
   router.post("/user/auth", authorize, authCallback);
   router.post("/user/logout", authorize, logout);
 };
