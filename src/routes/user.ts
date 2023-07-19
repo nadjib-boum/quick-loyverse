a5bec4abf73b54f5d55c842abdda16b6278ef8af
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-19 19:40:12 +0400

    add login page

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import type { Router } from "express";
 import { authorize, validateLogin } from "../middlewares/user";
-import { login, authCallback, logout } from "../controllers/user";
+import { login, authCallback, logout, renderLogin } from "../controllers/user";
 
 export default (router: Router) => {
+  router.get("/user/login", renderLogin);
   router.post("/user/login", validateLogin, login);
   router.get("/user/auth", authorize, authCallback);
   router.get("/user/logout", authorize, logout);
