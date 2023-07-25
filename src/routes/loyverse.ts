37142276df4f79e632a2719e218b32ec35b77a49
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-25 19:15:33 +0400

    add getories router

--- src/routes/loyverse.ts
+++ src/routes/loyverse.ts
@@ -1,7 +1,8 @@
 import type { Router } from "express";
-import { loyverseAuth } from "../controllers/loyverse";
+import { getCatgories, loyverseAuth } from "../controllers/loyverse";
 import { validate_loyverseAuth } from "../middlewares/loyverse";
 
 export default (router: Router) => {
   router.post("/loyverse/auth/:companyId", validate_loyverseAuth, loyverseAuth);
+  router.get("/loyverse/categories", getCatgories);
 };
