538c91bcc28f906e8259f145cec7f231ff3b3492
nadjib-boum <nadjib.dev21@gmail.com>
2023-07-26 19:23:22 +0400

    change loyverse route path

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -13,6 +13,10 @@ import {
 export default (router: Router) => {
   router.get("/companies", getAllCompanies);
   router.get("/companies/:account_sub", getCompaniesByAccount);
-  router.post("/loyverse/auth/:companyId", validate_loyverseAuth, loyverseAuth);
+  router.post(
+    "/companies/loyverse/:companyId",
+    validate_loyverseAuth,
+    loyverseAuth
+  );
   router.put("/companies/:id", validateTokenRefresh, refreshAccessToken);
 };
