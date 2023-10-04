084cd4463b9b9b4e82bd65fa151dd3913d1e4fd2
nadjib-boum <nadjib.dev22@gmail.com>
2023-10-04 18:08:10 +0400

    protect accounts route

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -1,8 +1,9 @@
 import type { Router } from "express";
 import { getAllAccounts, getCompaniesByAccount } from "../controllers/accounts";
 import { validateId } from "../middlewares/validate";
+import { authorize } from "../middlewares/user";
 
 export default (router: Router) => {
-  router.get("/accounts", getAllAccounts);
-  router.get("/accounts/:id", validateId, getCompaniesByAccount);
+  router.get("/accounts", authorize, getAllAccounts);
+  router.get("/accounts/:id", [authorize, validateId], getCompaniesByAccount);
 };
